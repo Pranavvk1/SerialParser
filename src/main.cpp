87475b8c3a501a6f305763fdddf1d19b422397f6
Pranav Kulkarni <pranavkulkarni2104@gmail.com>
2026-01-05 20:30:03 -0500

    reading ints, with bugs

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 #include <vector>
 
-/*two inputs- clk and x
+/*two inputs- clk and x (on input_pin)
 *int vector- used to store parsed ints.
 *counter used to count till 3 - increment whenever clk goes high
 */
 
 std::vector<int> intVec;
-int clk_pin = 0;
-int input_pin = 0;
+int clk_pin = 2;
+int input_pin = 15;
 int end_signal_pin = 0;
 
 void setup() {
@@ -16,12 +16,32 @@ void setup() {
   pinMode(clk_pin, INPUT);
   pinMode(input_pin, INPUT);
   pinMode(end_signal_pin, INPUT);
+  Serial.begin(115200);
 }
 
 int counter = 0;
+bool prevClk = false;
 
 void loop() {
-  // put your main code here, to run repeatedly
+  // put your main code here, to run repeatedly:
+  int num = 0;
+  while (counter <= 2) {
+    if(digitalRead(clk_pin) && !prevClk) {
+      if(digitalRead(input_pin)) {
+        num += 1 << counter;
+      }
+      counter++;
+      prevClk = true;
+    } else if(!digitalRead(clk_pin)) {
+      prevClk = false;
+    }
+  }
+  intVec.push_back(num);
 
+  if(digitalRead(end_signal_pin)){
+    for(int i : intVec) {
+      Serial.println(i);
+    }
+  }
 }
 
